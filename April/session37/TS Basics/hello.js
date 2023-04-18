e1fafdbe95598cb2baef53c73a5b5f07467b29e7
ManjunathSaukar <manjunathsaukar@gmail.com>
2023-04-18 11:38:44 +0530

    Updated session 37 with classwork and assignments

---
+++ April/session37/TS Basics/hello.js
@@ -0,0 +1,6 @@
+var timeOutMilliSeconds = 3000;
+setTimeout(function () {
+    console.log("Welcome to TypeScript");
+}, timeOutMilliSeconds);
+console.log("valid js is valid ts");
+console.log("end of program");
---
+++ April/session37/TS Basics/hello.ts
@@ -0,0 +1,7 @@
+const timeOutMilliSeconds:number = 3000;
+setTimeout(() =>
+{
+    console.log("Welcome to TypeScript");
+}, timeOutMilliSeconds);
+console.log("valid js is valid ts");
+console.log("end of program");
